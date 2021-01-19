074e06cfd6dce0bccd12bd7b40623aa94f1e601a
LiZekun <AarLee2954@163.com>
2021-01-19 11:25:50 +0800

    add module: Demo and update .gitignore - add ignore rules for .iml

---
+++ Demo/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/***
+ * @Program: ScratchPaper
+ * @Description: hello world sample
+ * @Author: Lzk
+ * @Create: 11:03 2021/01/19
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
