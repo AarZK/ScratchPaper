e5a8f1dfdc240dd3b32ae3ef1bbd4a6d27e60916
LiZekun <AarLee2954@163.com>
2021-01-19 12:07:04 +0800

    update me.lzk.demo.helloworld.HelloWorld - update default comment of Class and method

--- Demo/src/main/java/me/lzk/demo/helloworld/HelloWorld.java
+++ Demo/src/main/java/me/lzk/demo/helloworld/HelloWorld.java
@@ -4,10 +4,17 @@ package me.lzk.demo.helloworld;
  * @Program: ScratchPaper
  * @Description: hello world sample
  * @Author: Li Zekun
- * @Create: 11:03 2021/01/19
+ * @CreateDate: 11:03 2021/01/19
  **/
 public class HelloWorld {
-    
+
+    /**
+     * @Description:
+     * @Param: [args]
+     * @Return: void
+     * @Editor: Li Zekun
+     * @LastEdit: 12:03 2021/1/19
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
