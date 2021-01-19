0e098d4c7486275229a33f0fb30aaee5ff77e64a
LiZekun <AarLee2954@163.com>
2021-01-19 11:53:14 +0800

    update me.lzk.demo.helloworld.HelloWorld - update default author to 'Li Zekun'

---
+++ Demo/src/main/java/me/lzk/demo/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package me.lzk.demo.helloworld;
+
+/***
+ * @Program: ScratchPaper
+ * @Description: hello world sample
+ * @Author: Li Zekun
+ * @Create: 11:03 2021/01/19
+ **/
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
